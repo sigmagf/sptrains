a02bd166346e1093ffe878ef38252ee4be8642b1
Joao Gabriel <jgfurlan2@gmail.com>
2020-09-12 16:37:28 -0300

    Fix bugs 2

--- src/pages/api/status.ts
+++ src/pages/api/status.ts
@@ -8,21 +8,17 @@ import { IStatusResponse, ILine } from '~/interfaces';
 export default async (req: NowRequest, res: NowResponse) => {
   const lineColors = useLineColor();
 
-  const result = await axios.get<IStatusResponse[]>(process.env.API_URL);
+  axios.get<IStatusResponse[]>(process.env.API_URL).then((result) => {
+    const lines = result.data.map((line): ILine => ({
+      id: line.LinhaId,
+      name: line.Nome,
+      color: lineColors[line.LinhaId],
+      status: line.Status,
+      details: line.Descricao,
+      operator: line.Tipo,
+      updatedAt: line.DataGeracao,
+    }));
 
-  if(!result.data) {
-    throw new Error('Error on get status');
-  }
-
-  const lines = result.data.map((line): ILine => ({
-    id: line.LinhaId,
-    name: line.Nome,
-    color: lineColors[line.LinhaId],
-    status: line.Status,
-    details: line.Descricao,
-    operator: line.Tipo,
-    updatedAt: line.DataGeracao,
-  }));
-
-  return res.json(lines);
+    return res.json(lines);
+  }).catch((error) => res.json({ message: error.message || 'Unexpected error.' }));
 };

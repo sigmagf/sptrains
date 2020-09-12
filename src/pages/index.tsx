f246939d487e75762550e684180e096435e30187
Joao Gabriel <jgfurlan2@gmail.com>
2020-09-12 16:43:34 -0300

    Fix bugs 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,17 +14,26 @@ interface IIndexProps {
   lines: ILine[];
 }
 
+interface IRequestError {
+  code: number;
+  message: string;
+}
+
 const apiURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/status' : '/api/status';
 
 const Index: NextPage<IIndexProps> = ({ lines }) => {
   const [linesStates, setLinesStates] = useState<ILine[]>(lines);
+  const [requestError, setRequestError] = useState<IRequestError>(null);
 
-  const getData = useCallback(async () => {
-    const result = await axios.get<ILine[]>(apiURL);
-
-    if(result.data) {
+  const getData = useCallback(() => {
+    axios.get<ILine[]>(apiURL).then((result) => {
       setLinesStates(result.data);
-    }
+    }).catch((err) => {
+      setRequestError({
+        code: err.response.status || 0,
+        message: err.response.message || err.message || 'Unexpected error',
+      });
+    });
   }, []);
 
   useEffect(() => {
@@ -43,7 +52,15 @@ const Index: NextPage<IIndexProps> = ({ lines }) => {
         <title>SPTrains</title>
       </Head>
       <IndexContainer>
-        {linesStates.map((line) => <LineCard key={line.id} line={line} />)}
+        {requestError && (
+          <div>
+            <h2>Request Error.</h2>
+            <h3>Code: {requestError.code}</h3>
+            <h5>Message: {requestError.message}</h5>
+          </div>
+
+        )}
+        {!requestError && linesStates.map((line) => <LineCard key={line.id} line={line} />)}
       </IndexContainer>
     </>
   );
